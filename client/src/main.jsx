f96e7775576dc9b0626dc27e41b83feaff3de452
unnati06 <137003791+unnati06@users.noreply.github.com>
2024-09-30 21:45:30 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,6 @@ import { ContextProvider, SocketContext } from './constants/SocketContext';
 import { AuthProvider } from './constants/AuthContext';
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 
 ReactDOM.render(
     <AuthProvider>
